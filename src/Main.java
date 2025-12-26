64b2209ad74904c89089dbf942ef66ef16b6369e
Charles Benedict C. Sual <charles_benedict_sual@dlsl.edu.ph>
2025-12-26 21:49:02 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+     User user1 = new User("blackgamer60", "12345678", false);
+User user2 =  new User("IshaJ", "securegirlalooo9", true);
+
+        System.out.println("Username: " + user1.getUserName() + " |  Status : " + user1.checkStatus());
+        user2.deActivateAcc();
+        System.out.println("Username: " + user2.getUserName() + " |  Status : " + user2.checkStatus());
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+
+public class User {
+
+    private String userName;
+    private String password;
+    private boolean active;
+    Scanner input = new Scanner(System.in);
+
+    User(String userName, String password, boolean active) {
+        this.userName = userName;
+        this.password = password;
+        this.active = active;
+    }
+
+
+
+    public void setUserName(String newUsername) {
+        userName = newUsername;
+
+        if(newUsername  != null && !newUsername.isEmpty()){
+            userName = newUsername;
+
+        }
+    }
+
+
+    public void setPassword(String newPassword) {
+
+        if(newPassword != null && newPassword.length() >= 8){
+            password = newPassword;
+            System.out.println("Password Successfully Changed!");
+        }
+    }
+
+
+    public String getUserName() {
+      return userName;
+    }
+
+
+    public String getPassword() {
+       return password;
+    }
+
+
+
+    public void activateAcc(){
+        if(active){
+            System.out.println("Account is already active!");
+        }else{
+            active = true;
+            System.out.println("Account activated!");
+        }
+    }
+
+    public void deActivateAcc(){
+        if(!active){
+            System.out.println("Account is already inactive!");
+        }else{
+            active = false;
+            System.out.println("Account Deactivated!");
+        }
+    }
+
+
+
+        public String checkStatus() {
+            if(active){
+                return "Active";
+            }else{
+                return "Inactive";
+            }
+        }
+
+
+    }
+
