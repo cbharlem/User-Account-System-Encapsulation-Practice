12d2c643448fe10b3bdc3d097e33b79e66c1e5f0
Charles Benedict C. Sual <charles_benedict_sual@dlsl.edu.ph>
2025-12-26 23:07:26 +0800

    Added 3rd User

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
      User user1 = new User("blackgamer60", "12345678", false);
 User user2 =  new User("IshaJ", "securegirlalooo9", true);
+User user3 = new User("Finn", "password123", false);
 
         System.out.println("Username: " + user1.getUserName() + " |  Status : " + user1.checkStatus());
         user2.deActivateAcc();
